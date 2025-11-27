c1d844bf39f1e9489b57cb81c5708f7f89934af7
charles-masse <charlesmasseqc@gmail.com>
2025-11-27 13:49:05 -0500

    Updating currentFrame and delta transforms for easier debugging

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 import maya.cmds as cmds
 
-SAVE_PATH = 'D:/Desktop/test.png'
+SAVE_PATH = 'D:/Desktop/Scripts/portfolio/public/textures/animations.png'
 
 def lerpFactor(a, b, value):
     return (value - a) / (b - a)
@@ -21,31 +21,35 @@ def run():
 
     vertex_animation = []
 
-    selected_mesh = cmds.ls(sl=1, typ='transform')[0]
+    selected_mesh = cmds.ls('anim', typ='transform')[0]
     selected_mesh_vertices = cmds.ls(selected_mesh + ".vtx[*]", fl=1)
 
     start_frame = int(cmds.playbackOptions(q=1, ast=1))
     end_frame = int(cmds.playbackOptions(q=1, aet=1))
     for frame in range(start_frame, end_frame + 1):
+        cmds.currentTime(frame, e=1)
 
         frame_vertex_positions = []
 
         for vertexId, vertex in enumerate(selected_mesh_vertices):
+            bind_position = cmds.xform('bind.' + vertex.split('.')[-1], q=1, t=1)
             vertex_position = cmds.xform(vertex, q=1, t=1)
-            frame_vertex_positions.extend(vertex_position)
+            frame_vertex_positions.extend([vertex_position[0] - bind_position[0], vertex_position[1] - bind_position[1], vertex_position[2] - bind_position[2]])
 
         vertex_animation.extend(frame_vertex_positions)
 
     min_transform = min(vertex_animation)
     max_transform = max(vertex_animation)
 
+    print(min_transform, max_transform)
+
     vertex_animation_normalized = [int(lerpFactor(min_transform, max_transform, transform) * 255) for transform in vertex_animation]
     
-    vertex_animation_bundled = []
+    vertex_animation_vec3 = []
     for i in range(0, len(vertex_animation_normalized), 3):
-        vertex_animation_bundled.append(tuple(vertex_animation_normalized[i:i+3]))
+        vertex_animation_vec3.append(tuple(vertex_animation_normalized[i:i+3]))
 
-    return generateTexture(vertex_animation_bundled, len(selected_mesh_vertices), end_frame - start_frame + 1)
+    return generateTexture(vertex_animation_vec3, len(selected_mesh_vertices), end_frame - start_frame + 1)
 
 run().save(SAVE_PATH)
 webbrowser.open(SAVE_PATH)
